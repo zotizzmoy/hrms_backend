cadb572fef8f664c5202c849cf0c11487518b67e
zotizzmoy <zotizzmoydeka@gmail.com>
2023-08-09 16:59:29 +0530

    fix

--- middleware/upload_middleware.js
+++ middleware/upload_middleware.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
         cb(null, './public/uploads');
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.fieldname);
+        cb(null, Date.now() + '-' + file.fieldname.jpeg);
     }
 });
 const fileFilter = function (req, file, cb) {
