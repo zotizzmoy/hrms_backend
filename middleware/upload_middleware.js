b7252b95db700c21f384a8e7dd3671d12d1faba0
zotizzmoy <zotizzmoydeka@gmail.com>
2023-08-11 13:11:19 +0530

    fix

--- middleware/upload_middleware.js
+++ middleware/upload_middleware.js
@@ -25,7 +25,7 @@ const moveImage = async (req, res, next) => {
     if (req.file) {
         const file = req.file;
         const outputFilename = file.filename;
-        const destinationPath = 'public/uploads/' + outputFilename;
+        const destinationPath = './public/uploads/' + outputFilename;
 
         try {
             const resizedBuffer = await sharp(file.path)
@@ -46,7 +46,7 @@ const moveImage = async (req, res, next) => {
 
         for (const file of req.files) {
             const outputFilename = file.filename;
-            const destinationPath = 'public/uploads/' + outputFilename;
+            const destinationPath = './public/uploads/' + outputFilename;
 
             try {
                 const resizedBuffer = await sharp(file.path)
