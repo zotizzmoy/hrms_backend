f65774a462be29115d8f4ca1d7c509691864109a
zotizzmoy <zotizzmoydeka@gmail.com>
2023-08-11 13:47:03 +0530

    bug fixed

--- middleware/upload_middleware.js
+++ middleware/upload_middleware.js
@@ -1,75 +1,25 @@
 const multer = require('multer');
-const sharp = require('sharp');
-const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/uploads');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.fieldname);
+    destination: './public/uploads/',
+    filename: (req, file, callback) => {
+        const ext = path.extname(file.originalname);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        callback(null, 'image-' + uniqueSuffix + ext);
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
+const fileFilter = (req, file, callback) => {
+    const allowedTypes = ['image/jpeg', 'image/png'];
+
+    if (allowedTypes.includes(file.mimetype)) {
+        callback(null, true);
     } else {
-        cb(new Error('Only images are allowed!'));
+        callback(new Error('Only JPEG and PNG files are allowed.'));
     }
 };
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({ storage, fileFilter });
 
-const moveImage = async (req, res, next) => {
-    if (req.file) {
-        const file = req.file;
-        const outputFilename = file.filename;
-        const destinationPath = './public/uploads/' + outputFilename;
-
-        try {
-            const resizedBuffer = await sharp(file.path)
-                .resize(800) // You can adjust the desired dimensions here
-                .toBuffer();
-
-            fs.writeFileSync(destinationPath, resizedBuffer);
-
-            fs.unlinkSync(file.path); // Delete the original unresized image
-
-            req.compressedFiles = [outputFilename];
-            next();
-        } catch (error) {
-            next(error);
-        }
-    } else if (req.files && req.files.length > 0) {
-        req.compressedFiles = [];
-
-        for (const file of req.files) {
-            const outputFilename = file.filename;
-            const destinationPath = './public/uploads/' + outputFilename;
-
-            try {
-                const resizedBuffer = await sharp(file.path)
-                    .resize(800) // You can adjust the desired dimensions here
-                    .toBuffer();
-
-                fs.writeFileSync(destinationPath, resizedBuffer);
-
-                fs.unlinkSync(file.path); // Delete the original unresized image
-
-                req.compressedFiles.push(outputFilename);
-            } catch (error) {
-                next(error);
-                return;
-            }
-        }
-
-        next();
-    } else {
-        next();
-    }
-};
-
-
-
-module.exports = { upload, moveImage };
+module.exports = upload;
