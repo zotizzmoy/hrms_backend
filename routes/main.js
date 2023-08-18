428b96c7e53980582c15bd45f5bdaa3c523feb59
zotizzmoy <zotizzmoydeka@gmail.com>
2023-08-18 16:53:55 +0530

    new_bug_fix

--- routes/main.js
+++ routes/main.js
@@ -116,7 +116,7 @@ router.route('/update-education-details/:id').post(UserController.updateUserEduc
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //HelperController
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-router.route('/get-leave-deductions').post(HelperController.getLeaveDeductions);
+router.route('/get-leave-deductions').post(HelperController.getAllLeaveDeductions);
 
 
 
